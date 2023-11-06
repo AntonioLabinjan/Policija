c45e89ec8eff21f601bcf1ba1f9cde3c2df28688
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2023-11-06 10:41:33 +0100

    Update policija.sql

--- policija.sql
+++ policija.sql
@@ -181,6 +181,117 @@ CREATE TABLE Sui_slucaj (
 
 
 # TRIGERI
+# Iako postoji opcija kaskadnog brisanja u SQL-u, ovdje ćemo u nekim slučajevima pomoću trigera htijeti zabraniti brisanje, pošto je važno da neki podaci ostanu zabilježeni. U iznimnim slučajevima možemo ostavljati obavijest da je neka vrijednost obrisana iz baze. Također, u većini slučajeva nam opcija kaskadnog brisanja nikako ne bi odgovarala, zato što je u radu policije važna kontinuirana evidencija
+# Napiši triger koji će a) ako u području uprave više od 5 mjesta, zabraniti brisanje uz obavijest: "Područje uprave s više od 5 mjesta ne smije biti obrisano" b) ako u području uprave ima manje od 5 mjesta, dopustiti da se područje uprave obriše, ali će se onda u mjestima koja referenciraju to područje uprave, pojaviti obavijest "Prvotno područje uprave je obrisano, povežite mjesto s novim područjem"
+DELIMITER //
+CREATE TRIGGER provjeri_podrucje_uprave_prije_brisanja
+BEFORE DELETE ON Podrucje_uprave
+FOR EACH ROW
+BEGIN
+    DECLARE count_mjesta INT;
+    SELECT COUNT(*) INTO count_mjesta FROM Mjesto WHERE id_podrucje_uprave = OLD.id;
+    
+    IF count_mjesta > 5 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Područje uprave s više od 5 mjesta ne smije biti obrisano.';
+    ELSE
+        UPDATE Mjesto
+        SET id_podrucje_uprave = NULL
+        WHERE id_podrucje_uprave = OLD.id;
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Prvotno područje uprave je obrisano, povežite mjesto s novim područjem.';
+    END IF;
+END;
+//
+DELIMITER ;
+
+# Napiši triger koji će a) spriječiti brisanje osobe ako je ona zaposlenik koji je još u službi (datum izlaska iz službe nije null) uz obavijest:
+# "osoba koju pokušavate obrisati je zaposlenik, prvo ju obrišite iz tablice zaposlenika)" b) obrisati osobu i iz tablice zaposlenika i iz tablice osoba, 
+# ukoliko datum_izlaska_iz_službe ima neku vrijednost što ukazuje da osoba više nije zaposlena
+DELIMITER //
+CREATE TRIGGER provjeri_osobu_prije_brisanja
+BEFORE DELETE ON Osoba
+FOR EACH ROW
+BEGIN
+    DECLARE is_zaposlenik BOOLEAN;
+    SET is_zaposlenik = EXISTS (SELECT 1 FROM Zaposlenik WHERE id_osoba = OLD.id AND datum_izlaska_iz_sluzbe IS NULL);
+
+    IF is_zaposlenik = TRUE THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Osoba koju pokušavate obrisati je zaposlenik, prvo ju obrišite iz tablice Zaposlenik.';
+    ELSE
+        IF OLD.datum_izlaska_iz_sluzbe IS NOT NULL THEN
+            DELETE FROM Zaposlenik WHERE id_osoba = OLD.id;
+        END IF;
+    END IF;
+END;
+//
+DELIMITER ;
+
+# Napiši triger koji će, u slučaju da se kažnjivo djelo obriše iz baze, postaviti id_kaznjivo_djelo kod psa na NULL, ukoliko je on prije bio zadužen za upravo to KD koje smo obrisali
+DELIMITER //
+CREATE TRIGGER azuriraj_psa
+AFTER DELETE ON Kaznjiva_djela
+FOR EACH ROW
+BEGIN
+    UPDATE Pas
+    SET id_kaznjivo_djelo = NULL
+    WHERE id_kaznjivo_djelo = OLD.id;
+END;
+//
+DELIMITER ;
+
+# Napiši triger koji će zabraniti da iz tablice obrišemo predmete koji služe kao dokazi u aktivnim slučajevima (status im nije završeno, te se ne nalaza u arhivi) uz obavijest "Ne možete obrisati dokaze za aktivan slučaj"
+DELIMITER //
+CREATE TRIGGER ne_brisi_akitvne_dokaze
+BEFORE DELETE ON Predmet
+FOR EACH ROW
+BEGIN
+    DECLARE aktivan INT;
+    SELECT COUNT(*) INTO aktivan FROM Slucaj WHERE id_dokaz = OLD.id AND status != 'Završeno';
+    
+    IF aktivan > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Ne možete obrisati dokaze za aktivni slučaj.';
+    END IF;
+END;
+//
+DELIMITER ;
+
+# Napiši triger koji će zabraniti da iz tablice obrišemo osobe koje su evidentirani kao počinitelji u aktivnim slučajevima
+DELIMITER //
+CREATE TRIGGER ne_brisi_pocinitelje
+BEFORE DELETE ON Osoba
+FOR EACH ROW
+BEGIN
+    DECLARE je_pocinitelj INT;
+    SELECT COUNT(*) INTO je_pocinitelj FROM Slucaj WHERE id_pocinitelj = OLD.id AND status != 'Završeno';
+    
+    IF je_pocinitelj > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Ne možete obrisati osobu koja je evidentirana kao počinitelj.';
+    END IF;
+END;
+//
+DELIMITER ;
+
+# Napiši triger koji će zabraniti brisanje bilo kojeg izvještaja kreiranog za slučajeve koji nisu završeni (završetak je NULL), ili im je završetak "noviji" od 10 godina (ne smijemo brisati izvještaje za aktivne slučajeve, i za slučajeve koji su završili pred manje od 10 godina)
+DELIMITER //
+CREATE TRIGGER prevent_delete_izvjestaji
+BEFORE DELETE ON Izvjestaji
+FOR EACH ROW
+BEGIN
+    DECLARE slucaj_zavrsen DATETIME;
+    SELECT zavrsetak INTO slucaj_zavrsen FROM Slucaj WHERE id = OLD.id_slucaj;
+    
+    IF slucaj_zavrsen IS NULL OR slucaj_zavrsen > DATE_SUB(NOW(), INTERVAL 10 YEAR) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Ne možete obrisati izvještaj za aktivan slučaj ili za slučaj koji je završio unutar posljednjih 10 godina.';
+    END IF;
+END;
+//
+DELIMITER ;
+
 # Triger koji osigurava da pri unosu spola osobe možemo staviti samo muški ili ženski spol
 DELIMITER //
 CREATE TRIGGER Provjeri_Ispravnost_Spola
@@ -1399,14 +1510,15 @@ DELIMITER ;
 
 /* KILLCOUNT:
     18 tables
-    9 triggers
+    15 triggers
     18 queries
     13 views
     7 functions
     27 procedures
 */
 
-CALL Dodaj_Novo_Kaznjivo_Djelo('Kaznivo Djelo 1', 'Opis prvog kaznenog djela', 1000);
+# Ovo je samo neko testiranje, niš bitno
+/*CALL Dodaj_Novo_Kaznjivo_Djelo('Kaznivo Djelo 1', 'Opis prvog kaznenog djela', 1000);
 CALL Dodaj_Novo_Kaznjivo_Djelo('Kaznivo Djelo 2', 'Opis drugog kaznenog djela', 1500);
 CALL Dodaj_Novo_Kaznjivo_Djelo('Kaznivo Djelo 3', 'Opis trećeg kaznenog djela', 800);
 
@@ -1414,3 +1526,4 @@ SELECT * FROM kaznjiva_djela;
 SET SQL_SAFE_UPDATES = 0;
 
 DELETE FROM kaznjiva_djela;
+*/

fa61eb7772ee86b770daf24173838de1de815e07
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2023-12-13 22:07:05 +0100

    Update Program_za_prebrajat_koliko_čega_ima.py

--- Program_za_prebrajat_koliko_čega_ima.py
+++ Program_za_prebrajat_koliko_čega_ima.py
@@ -1,7 +1,7 @@
 # Ovaj program postoji samo zato da se lakše vodi onu evidenciju na dnu sql fajla. Poželjno je ne predat ga kao dio projekta :)
 
 def broj_rijeci_u_tekstu(text, keywords):
-  rez = {key: text.lower().count(key.lower()) for key in keywords
+  rez = {key: text.lower().count(key.lower()) for key in keywords}
          return rez
 
 print("Paste your text:")
